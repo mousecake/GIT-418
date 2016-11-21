7f353438f06696ec69e006de78c9411ae7034444
mousecake <msearcy8@yahoo.com>
2016-11-20 22:16:29 -0700

    delete this
    deleted stuff

---
+++ scripts/resources.js
@@ -0,0 +1,16 @@
+var resources [
+{'resource': 'Career Preparation',		'url': 'https://students.asu.edu/careerguide/careerpreparation'},
+{'resource': 'College Advising',		'url': 'https://http://poly.engineering.asu.edu/advising'},
+{'resource': 'Counseling Services',		'url': 'https://students.asu.edu/counseling'},
+{'resource': 'Financial Aid',			'url': 'https://students.asu.edu/financialaid'},
+{'resource': 'Fitness & Recreation',	'url': 'http://fitness.asu.edu/'},
+{'resource': 'Health Services',			'url': 'https://students.asu.edu/health'},
+{'resource': 'Housing',					'url': 'http://housing.asu.edu/home'},
+{'resource': 'Library',					'url': 'http://lib.asu.edu/'},
+{'resource': 'Parking',					'url': 'https://cfo.asu.edu/pts'},
+{'resource': 'Scholarships',			'url': 'https://students.asu.edu/scholarships'},
+{'resource': 'Student Employment',		'url': 'https://students.asu.edu/employment'},
+{'resource': 'Student Organizations',	'url': 'http://asu.orgsync.com/'},
+{'resource': 'Tutoring Support',		'url': 'https://tutoring.asu.edu/'},
+{'resource': 'Writing Support',			'url': 'https://tutoring.asu.edu/writing-centers'}
+]
